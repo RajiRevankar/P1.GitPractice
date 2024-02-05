6c6b1827cd3bf518178f8de374b7a440e2e4d2f0
Raji_ <Raji_@rajeshwari>
2024-02-05 12:13:28 +0530

    dummy class updated and commited

--- src/test/java/sample/Dummy.java
+++ src/test/java/sample/Dummy.java
@@ -4,6 +4,7 @@ public class Dummy {
 	public static void main(String[] args) {
 		System.out.println("hello friends");
 		System.out.println("how r u");
+		System.out.println("see you tomorrow");
 	}
 
 }
